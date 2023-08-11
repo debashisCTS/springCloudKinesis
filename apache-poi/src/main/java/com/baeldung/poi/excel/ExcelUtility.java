82cf15673e4e5dab649ba80b1f68c64dd03e5c7c
debashisCTS <51178137+debashisCTS@users.noreply.github.com>
2023-08-11 20:10:40 +0530

    secret

--- apache-poi/src/main/java/com/baeldung/poi/excel/ExcelUtility.java
+++ apache-poi/src/main/java/com/baeldung/poi/excel/ExcelUtility.java
@@ -20,7 +20,9 @@ public class ExcelUtility {
         FileInputStream inputStream = null;
         StringBuilder toReturn = new StringBuilder();
 		
-		String AWS_ACCESS_KEY_ID = "AKIA46DT2LZZH6YNYZZN";
+		String aws_access_key_id=AKIA46DT2LZZH6YNYZZN;
+        String aws_secret_access_key=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;
+	
 				
 azure
         try {
